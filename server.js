aa77e106bd3339d94ae89d044cce7eefcb72aa5e
DimitarYosifov <yosifov1985@gmail.com>
2022-01-30 11:02:17 +0200

    initial test2

--- server.js
+++ server.js
@@ -3,20 +3,12 @@ const bodyParser = require('body-parser');
 const path = require('path');
 // const router = express.Router();
 const app = express();
-// const bcrypt = require('bcryptjs');
+const bcrypt = require('bcryptjs');
 const firebase = require("firebase/app");
 require('firebase/auth');
 require('firebase/database');
 let port = process.env.PORT || 8000;
-// app.set('views', path.join(__dirname, ''));
-// app.disable('x-powered-by');
-// app.use(express.static(__dirname + '/'));
 app.use(bodyParser.json());
-// app.use(router);
-
-// app.set('view engine', 'html');
-// app.engine('html', require('ejs').renderFile);
-
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -33,8 +25,7 @@ app.listen(port, function () { console.log(`app started on port: ${port}`);});
 
 firebase.initializeApp(config_firebase);
 
-
-app.post('/test', async (req, res) => {
+app.post('/getAllClubsData', async (req, res) => {
     firebase.database().ref("/clubs/").once('value').then(function (snapshot) {
         res.set('Content-Type', 'application/json');
         res.status(200);
